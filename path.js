5e096770827bdec49140953353ec3c32c8e09af4
Radim Vrška <87022843+radim567@users.noreply.github.com>
2023-06-26 16:26:29 +0200

    path module done

--- path.js
+++ path.js
@@ -9,3 +9,41 @@ const base2 = path.basename(myFilePath, '.js');
 
 console.log(base1);
 console.log(base2);
+
+// extname()
+
+const ext = path.extname(myFilePath);
+console.log(ext);
+
+// dirname()
+
+const dir = path.dirname(myFilePath);
+console.log(dir);
+
+// join()
+const myPath = path.join(
+  'home',
+  'username',
+  'Documents',
+  'Files',
+  'Node.js',
+  'path.js'
+);
+console.log(myPath);
+
+// resolve() - absolut path
+
+const absPath = path.resolve(
+  'home',
+  'username',
+  'Documents',
+  'Files',
+  'Node.js',
+  'path.js'
+);
+console.log(absPath);
+
+// __dirname - environment variable - absolute path
+console.log(__dirname);
+// __filename - environment variable
+console.log(__filename);
